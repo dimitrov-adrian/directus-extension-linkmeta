c38a18def09abeabf77cb15b9d88486f580e693d
e01 <dimitrov.adrian@gmail.com>
2020-12-06 10:36:54 +0200

    update

--- src/interface.vue
+++ src/interface.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
 
-    <v-input :disabled="disabled" :value="localUrl" @input="inputUrlHandler" @change="changeUrlHandler" :placeholder="placeholder" >
+    <v-input :disabled="disabled"
+             :value="localUrl"
+             :placeholder="placeholder"
+             @input="inputUrlHandler"
+             @change="changeUrlHandler"
+    >
       <template #prepend>
-        <v-icon name="error" v-if="localUrl && !isUri(localUrl)" />
-        <v-icon name="verified" v-else-if="localUrl && value && value.url && value.url === localUrl" />
-        <v-icon name="link" v-else />
+        <v-icon name="error" v-if="localUrl && !isUri(localUrl)"/>
+        <v-icon name="verified" v-else-if="localUrl && value && value.url && value.url === localUrl"/>
+        <v-icon name="link" v-else/>
       </template>
       <template #append>
-        <v-icon v-if="canRefresh" name="sync" @click="refresh" v-tooltip="$t('update')" />
-        <v-icon v-else-if="isUri(localUrl)" name="done" @click="changeUrlHandlerFromLocal" v-tooltip="$t('update')" />
+        <v-icon v-if="canRefresh" name="sync" @click="refresh" v-tooltip="$t('update')"/>
+        <v-icon v-else-if="isUri(localUrl)" name="done" @click="changeUrlHandlerFromLocal" v-tooltip="$t('update')"/>
       </template>
     </v-input>
 
@@ -17,7 +22,7 @@
       {{ hasError }}
     </v-notice>
 
-    <v-skeleton-loader v-if="loading" />
+    <v-skeleton-loader v-if="loading"/>
     <v-list v-else-if="value && typeof value === 'object'" class="preview">
       <v-list-item
           v-for="previewItem in preview"
@@ -26,7 +31,7 @@
           :class="'preview-item-' + previewItem"
       >
         <template v-if="['image', 'logo'].includes(previewItem) && value[previewItem]">
-          <img :src="getImageUrl(value[previewItem])" rel="noopener" />
+          <img :src="getImageUrl(value[previewItem])" rel="noopener"/>
         </template>
         <template v-else-if="previewItem === 'url' && value[previewItem]">
           <a :href="value[previewItem]" target="_blank" rel="noopener">
@@ -41,7 +46,7 @@
         <template v-else-if="value[previewItem]">
           <code>{{ previewItem }} : </code>
           <var v-if="value[previewItem]">{{ value[previewItem] }}</var>
-          <value-null v-else />
+          <value-null v-else/>
         </template>
       </v-list-item>
     </v-list>
@@ -51,10 +56,9 @@
 
 <script>
 import validUrl from 'valid-url'
-const controller = new AbortController()
-const signal = controller.signal
 
 export default {
+  inject: ['system'],
   props: {
     value: {
       type: Object,
@@ -75,11 +79,7 @@ export default {
     preview: {
       type: Array,
       default: () => ['image', 'title', 'url'],
-    }
-  },
-
-  unmounted: function () {
-    controller.abort()
+    },
   },
 
   data: function () {
@@ -99,42 +99,38 @@ export default {
   },
 
   computed: {
-    canRefresh: function() {
+    canRefresh: function () {
       return this.value?.url && this.localUrl && this.localUrl === this.value?.url
-    }
+    },
   },
 
   methods: {
 
     isUri: validUrl.isWebUri,
 
-    refresh: function() {
+    refresh: function () {
       if (this.value?.url) {
         this.processUrl(this.value.url)
       }
     },
 
-    processUrlByMicrolink: function(url) {
-      return fetch('https://api.microlink.io?url=' + encodeURIComponent(url), {
-        signal: signal,
-        method: 'GET',
+    processUrlByMicrolink: function (url) {
+      return this.system.axios.get(`https://api.microlink.io?url=${encodeURIComponent(url)}`, {
         headers: {
           'Content-Type': 'application/json',
         },
       })
     },
 
-    processUrlByExtension: function(url) {
-      return fetch('/custom/directus-extension-linkpreview-endpoint?url=' + encodeURIComponent(url), {
-        signal: signal,
-        method: 'GET',
+    processUrlByExtension: function (url) {
+      return this.system.api.get(`/custom/directus-extension-linkpreview-endpoint?url=${encodeURIComponent(url)}`, {
         headers: {
           'Content-Type': 'application/json',
         },
       })
     },
 
-    processUrl: function(url) {
+    processUrl: function (url) {
       if (!this.isUri(url)) {
         this.hasError = 'Invalid URL'
         return
@@ -143,35 +139,31 @@ export default {
       this.loading = true
       this.hasError = false
 
-      const requester = (() => {
-        switch (this.$props.service) {
-          case 'microlink':
-            return this.processUrlByMicrolink
-          default:
-            return this.processUrlByExtension
-        }
-      })()
+      const requester = {
+        microlink: this.processUrlByMicrolink,
+      }[this.$props.service] || this.processUrlByExtension
 
       requester(url)
-        .then(response => response.json())
-        .then(response => {
-          this.loading = false
-          if (response.status === 'success' && response.data) {
-            return response.data
-          } else {
-            throw Error(response.message)
-          }
-        })
-        .then(data => {
-          this.localUrl = url
-          this.$emit('input', data)
-        })
-        .catch((error) => {
-          this.hasError = error.toString()
-        })
+          .then(response => response.data)
+          .then(response => {
+            this.loading = false
+            if (response.status === 'success' && response.data) {
+              return response.data
+            }
+            else {
+              throw Error(response.message)
+            }
+          })
+          .then(data => {
+            this.localUrl = url
+            this.$emit('input', data)
+          })
+          .catch((error) => {
+            this.hasError = error.toString()
+          })
     },
 
-    changeUrlHandler: function(event) {
+    changeUrlHandler: function (event) {
       const url = event.target.value.trim()
       this.localUrl = url
       if (this.value && this.value.url && this.value.url === url) {
@@ -180,25 +172,27 @@ export default {
       this.processUrl(url)
     },
 
-    changeUrlHandlerFromLocal: function() {
+    changeUrlHandlerFromLocal: function () {
       if (this.localUrl) {
         this.processUrl(this.localUrl)
       }
     },
 
-    inputUrlHandler: function(newValue) {
+    inputUrlHandler: function (newValue) {
       this.localUrl = newValue
     },
 
-    getImageUrl: function(imageObject) {
+    getImageUrl: function (imageObject) {
       if (typeof imageObject === 'string') {
         return imageObject
-      } else if (typeof imageObject === 'object' && imageObject.url) {
+      }
+      else if (typeof imageObject === 'object' && imageObject.url) {
         return imageObject.url
-      } else {
+      }
+      else {
         return null
       }
-    }
+    },
   },
 }
 </script>
@@ -207,23 +201,28 @@ export default {
 .preview {
   background-color: var(--v-card-background-color);
 }
+
 .preview a {
   color: var(--primary);
   text-decoration: underline;
 }
+
 .preview-item-image img {
   height: auto;
 }
+
 .preview-item-image img,
 .preview-item-iframe .iframe-wrapper-bound {
   width: 360px;
   max-width: 100%;
 }
+
 .preview-item-iframe .iframe-wrapper-bound .iframe-wrapper {
   position: relative;
   padding-bottom: 56.25%;
   height: 0;
 }
+
 .preview-item-iframe .iframe-wrapper-bound .iframe-wrapper >>> iframe {
   position: absolute !important;
   top: 0 !important;
