122968545d03f2d766dab3c9dc80a1b155a7f8e6
e01 <dimitrov.adrian@gmail.com>
2022-01-13 01:28:15 +0200

    Add cache controls to image proxy

--- endpoint/index.js
+++ endpoint/index.js
@@ -6,10 +6,7 @@ const got = require('got');
 const processUrl = require('./fetcher-metascraper.js');
 
 module.exports = defineEndpoint((router, { exceptions }) => {
-	router.get('/', linkMetaEndpoint);
-	router.get('/img', imgProxy);
-
-	async function linkMetaEndpoint(req, res, next) {
+	router.get('/', async (req, res, next) => {
 		if (!req?.accountability?.role) {
 			return next(new exceptions.ForbiddenException());
 		}
@@ -19,13 +16,13 @@ module.exports = defineEndpoint((router, { exceptions }) => {
 		}
 
 		try {
-			res.send(await processUrl(req.query.url.toString()));
+			return res.send(await processUrl(req.query.url.toString()));
 		} catch (error) {
 			return next(new exceptions.ServiceUnavailableException(error));
 		}
-	}
+	});
 
-	async function imgProxy(req, res, next) {
+	router.get('/img', async (req, res, next) => {
 		if (!req?.accountability?.role) {
 			return next(new exceptions.ForbiddenException());
 		}
@@ -38,22 +35,23 @@ module.exports = defineEndpoint((router, { exceptions }) => {
 			const data = await got(req?.query?.url, { decompress: true, http2: true });
 
 			if (data.headers['content-length']) {
-				// Limit to 1MB
-				if (parseInt(data.headers['content-length']) > 1024 * 1024) {
-					throw Error('Image is too big');
-				}
-
 				res.set('Content-Length', data.headers['content-length']);
 			}
 
 			if (data.headers['content-type']) {
 				res.set('Content-Type', data.headers['content-type']);
-				res.contentType(data.headers['content-type']);
 			}
 
-			res.end(data.rawBody, 'binary');
+			if (!data.headers['content-type']?.startsWith('image/')) {
+				return next(new exceptions.InvalidPayloadException());
+			}
+
+			res.set('Cache-Control', 'public, max-age=2592000');
+			res.set('Last-Modified', new Date().toGMTString());
+
+			return res.end(data.rawBody, 'binary');
 		} catch (error) {
 			return next(new exceptions.ServiceUnavailableException(error));
 		}
-	}
+	});
 });
